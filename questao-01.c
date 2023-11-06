07c9afe00781712c4796010fa15856f6682d13de
Alan Nascimento <alannascimento.contato@gmail.com>
2023-11-06 18:02:02 -0300

    funcao-concluida

--- questao-01.c
+++ questao-01.c
@@ -2,6 +2,20 @@
 
     void somando(int array[], int numeros){
         while (numeros > 1){
-            
+            for(int i = 0; i > numeros - 1; i += 2) {
+                array[i] = array[i] + array[i + 1];
+            }
+            numeros = (numeros + 1) / 2;
+            for (int i = 0; i < numeros; i++) {
+                printf("%d", array[i]);
+                if(i < numeros - 1) {
+                    printf(" ");
+                }
+            }
+            printf("\n");
         }
+    }
+
+    int main(){
+        
     }

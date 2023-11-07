1891e0bb7f8e7c84d84b5ca989cd11440db56fca
Alan Nascimento <alannascimento.contato@gmail.com>
2023-11-07 15:44:12 -0300

    questao1-concluida

--- questao-01.c
+++ questao-01.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 
-    void somando(int array[], int numeros){
-        while (numeros > 1){
-            for(int i = 0; i > numeros - 1; i += 2) {
-                array[i] = array[i] + array[i + 1];
-            }
-            numeros = (numeros + 1) / 2;
-            for (int i = 0; i < numeros; i++) {
-                printf("%d", array[i]);
-                if(i < numeros - 1) {
-                    printf(" ");
-                }
-            }
-            printf("\n");
+void printArray(int array[], int lista) {
+    for (int i = 0; i < lista; i++) {
+        printf("%d", array[i]);
+        if (i < lista - 1) {
+            printf(" ");
         }
     }
+    printf("\n");
+}
 
-    int main(){
-        int array[10];
+int main() {
+    int numeros[10];
 
-        for (int i = 0; i < 10; i++) {
-            scanf("%d", &array[i]);
-        }
-        for (int i = 0; i < 10; i++) {
-            printf("%d", array[i]);
-            if (i < 9) {
-                printf(" ");
-            }
-        }
-        printf("\n");
-
-        somando(array, 10);
-
-        return 0;
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &numeros[i]);
     }
+
+    printArray(numeros, 10);
+
+    int resultados[10];
+    int lista2 = 10;
+
+    for (int k = 0; k < 9; k++) {
+        for (int i = 0; i < lista2 - 1; i++) {
+            resultados[i] = numeros[i] + numeros[i + 1];
+        }
+        printArray(resultados, lista2 - 1);
+        for (int i = 0; i < lista2 - 1; i++) {
+            numeros[i] = resultados[i];
+        }
+        lista2--;
+    }
+
+    return 0;
+}
